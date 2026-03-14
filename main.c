93428abc073534a7fd4e51fcd6564c478a116a0d
preetham <saipreethamraj321@gmail.com>
2026-03-14 07:07:59 +0000

    added new prgram for build

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+struct m_owners
 {
-     printf("Hello Git!");
-      return 0; 
+	char m_name[20];
+	int m_price;
+};
+void list(struct m_owners *x)
+{
+	printf("enter ur mobile name:\n");
+	scanf("%s",&x->m_name);
+	printf("enter ur mobile price:\n");
+	scanf("%d",&x->m_price);
+}
+void display(struct m_owners x)
+{
+	printf("name = %s",x.m_name);
+	printf("price = %d",x.m_price);
+}
+void main()
+{
+	struct m_owners own1,own2;
+	list(&own1);
+	display(own1);
+	list(&own2);
+	display(own2);
 }
